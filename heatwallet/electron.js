99edcefd229d01ebead8cf0faf7e2ffb2bf0a17e
alex01011 <am777@yandex.ru>
2025-01-23 12:25:44 +0300

    Add Heatwallet dist

---
+++ heatwallet/electron.js
@@ -0,0 +1,109 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Heat Ledger Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * */
+
+const electron = require('electron')
+const path = require("path")
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const spawn = require('child_process').spawn;
+const fs = require('fs');
+const APP_DIR = path.join(__dirname,'..')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 800,
+    autoHideMenuBar: true,
+    icon:`${APP_DIR}/electron/icon.png`,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  mainWindow.loadURL(`file://${APP_DIR}/index.html`)
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+
+  electron.ipcMain.on('userData-is-where-request', (event, arg) => {
+    let userDataDir = app.getPath('userData')
+    event.sender.send('userData-is-here-reply', userDataDir)
+  })
+
+  mainWindow.webContents.on('did-finish-load', ()=>{
+    // inject the context menu logic
+    fs.readFile(path.join(__dirname, 'run-in-renderer.js'), 'utf8', function(err, contents) {
+      if (err)
+        console.log(err)
+      else
+        mainWindow.webContents.executeJavaScript(contents);
+    });
+  });
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+var run = function(args, done) {
+  var updateExe = path.resolve(path.dirname(process.execPath), '..', 'Update.exe');
+  spawn(updateExe, args, {
+    detached: true
+  }).on('close', done);
+};
+
+var check = function() {
+  if (process.platform === 'win32') {
+    var cmd = process.argv[1];
+    var target = path.basename(process.execPath);
+    if (cmd === '--squirrel-install' || cmd === '--squirrel-updated') {
+      run(['--createShortcut=' + target + ''], app.quit);
+      return true;
+    }
+    if (cmd === '--squirrel-uninstall') {
+      run(['--removeShortcut=' + target + ''], app.quit);
+      return true;
+    }
+    if (cmd === '--squirrel-obsolete') {
+      app.quit();
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = check();
